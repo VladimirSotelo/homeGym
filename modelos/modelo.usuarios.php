51c8232464a4ac3476f231414c4fd7a4f552608b
VladimirSotelo <vladysotelo0088@gmail.com>
2024-11-16 13:57:38 -0300

    cambios por commit anterior

--- modelos/modelo.usuarios.php
+++ modelos/modelo.usuarios.php
@@ -7,12 +7,7 @@ class ModeloUsuarios{
     {
         
         try {
-<<<<<<< HEAD
-            $usuarios = conexion::conectar()->prepare("SELECT * FROM usuarios AS u INNER JOIN clientes as C ON C.id_usuario = U.id_Usuario
-                INNER JOIN plan_entrenamiento AS pe on pe.id_planEntrenamiento = c.id_planEntrenamiento;");
-=======
             $usuarios = conexion::conectar()->prepare("SELECT * from clientes as c, usuarios as u, profesores as p where c.id_Usuario = u.id_Usuario or p.id_Usuario = u.id_Usuario");
->>>>>>> a360b2a45f390a963827d16e17436417aa6e7e97
             $usuarios->execute();
 
             return $usuarios->fetchAll(PDO::FETCH_ASSOC);
